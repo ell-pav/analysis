8d37c2be2fc9012362f29cf8d8d78cadf31aab50
ell-pav <elliot.pavesi.pro@gmail.com>
2025-02-21 14:04:38 +0100

    exemple test lint

--- tests/test_exemple.py
+++ tests/test_exemple.py
@@ -5,5 +5,5 @@ def addition(a, b):
 
 # Test avec pytest
 def test_addition():
-    assert addition(2, 3) == 5  # On vérifie si l'addition de 2 et 3 est égale à 5
-    assert addition(-1, 1) == 0  # Test avec des nombres négatifs
+    assert addition(2, 3) == 5 
+    assert addition(-1, 1) == 0
