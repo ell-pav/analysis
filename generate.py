78275f1920bf59991db916a4e353dd5c34a314c4
ell-pav <elliot.pavesi.pro@gmail.com>
2025-02-21 13:09:18 +0100

    flake

--- generate.py
+++ generate.py
@@ -7,6 +7,7 @@ df = pd.read_csv('data/insurance.csv')
 # Créer une instance de Faker
 fake = Faker()
 
+
 # Fonction pour générer un nom, un prénom et un email
 def generate_fake_data():
     nom = fake.last_name()      # Nom de famille
@@ -14,8 +15,10 @@ def generate_fake_data():
     email = fake.email()        # Email
     return pd.Series([nom, prenom, email])
 
+
 # Appliquer la fonction pour générer les colonnes
-df[['Nom', 'Prénom', 'Email']] = df.apply(lambda row: generate_fake_data(), axis=1)
+df[['Nom', 'Prénom', 'Email']] = df.apply(
+    lambda row: generate_fake_data(), axis=1)
 
 # Sauvegarder le nouveau dataset avec les colonnes ajoutées
 df.to_csv('data/insurance_fake.csv', index=False)
